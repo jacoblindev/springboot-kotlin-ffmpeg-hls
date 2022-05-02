5d82b0dcc25ea556ab8a71d018cba9813c146610
Jacob Lin <jacoblindev@gmail.com>
2022-05-02 19:03:32 +0800

    init: Initialized project with properties setup

---
+++ src/main/kotlin/videohlsdemo/VideohlsdemoApplication.kt
@@ -0,0 +1,11 @@
+package videohlsdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VideohlsdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<VideohlsdemoApplication>(*args)
+}
---
+++ src/test/kotlin/videohlsdemo/VideohlsdemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package videohlsdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VideohlsdemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

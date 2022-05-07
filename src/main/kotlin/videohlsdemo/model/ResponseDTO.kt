7455571467f418c51696bd09fb982cf68ec5e80f
Jacob Lin <jacoblindev@gmail.com>
2022-05-07 17:23:43 +0800

    refactor: remove unused class & add schema

--- src/main/kotlin/videohlsdemo/model/ResponseDTO.kt
+++
@@ -1,6 +0,0 @@
-package videohlsdemo.model
-
-data class ResponseDTO(val success: Boolean) {
-    var msg: String? = null
-    var data: String? = null
-}
--- src/main/kotlin/videohlsdemo/model/VideoInfo.kt
+++
@@ -1,3 +0,0 @@
-package videohlsdemo.model
-
-data class VideoInfo(val title: String, val m3u8: String, val poster: String)
